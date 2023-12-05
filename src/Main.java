78ae898bd3a884229acc1ceb0be6c6908e6f51dd
VosMovok <dimasnugraha46@gmail.com>
2023-12-05 15:49:11 +0800

    changed package name

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         GameWindow frame = new GameWindow();
-        frame.setSize(1600, 1000);
+        frame.setSize(1600, 800);
         Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation((int) (screensize.getWidth() / 2 - frame.getSize().getWidth() / 2),
                 (int) (screensize.getHeight() / 2 - frame.getSize().getHeight() / 2));
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setTitle("Scrabble-Fight-The-Game");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(3);
